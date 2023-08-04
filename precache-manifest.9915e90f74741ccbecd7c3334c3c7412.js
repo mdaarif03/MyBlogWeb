03fc5dcc4b4bfef215416bd3c45b32592740e7b4
mdaarif03 <mdaarifraza03@gmail.com>
2023-08-04 16:53:18 +0530

    Updates

---
+++ precache-manifest.9915e90f74741ccbecd7c3334c3c7412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "621a654c0f9a898540c0",
+    "url": "/WebBlog/static/css/main.0758cffc.chunk.css"
+  },
+  {
+    "revision": "621a654c0f9a898540c0",
+    "url": "/WebBlog/static/js/main.621a654c.chunk.js"
+  },
+  {
+    "revision": "1b740eeec909a0fab1ad",
+    "url": "/WebBlog/static/js/1.1b740eee.chunk.js"
+  },
+  {
+    "revision": "2a1a2b453e092001924b",
+    "url": "/WebBlog/static/js/runtime~main.2a1a2b45.js"
+  },
+  {
+    "revision": "1a73b252fbcf7939db87d916fd5bb0ac",
+    "url": "/WebBlog/index.html"
+  }
+];
